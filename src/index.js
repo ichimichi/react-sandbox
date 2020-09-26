762374a648059af396a68bcf5a76945c4a5d4a1a
ichimichi <ichimichilaribok@gmail.com>
2020-09-26 14:28:14 +0530

    current move is highlighted as bold

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ class Game extends React.Component {
                 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button onClick={() => this.jumpTo(move)} style={{ fontWeight: this.state.stepNumber === move? 'bold': 'normal'}}>{desc}</button>
                 </li>
             );
         });
