f0e8ac89ff0b37226a5fb36f7a80ca7a3f2f1ae7
ichimichi <ichimichilaribok@gmail.com>
2020-09-27 16:39:49 +0530

    prevent default

--- src/index.js
+++ src/index.js
@@ -2,42 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
+function ActionLink() {
+    function handleClick(e) {
+        e.preventDefault();
+        console.log('The link was clicked.');
     }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState((state, props)=>(
-            {
-                date : new Date()
-            }
-        ));
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
+    return (
+        <a href="#" onClick={handleClick}>
+            Click me
+        </a>
+    );
 }
 
 ReactDOM.render(
-    <Clock />,
+    <ActionLink/>,
     document.getElementById('root')
-);
+)
