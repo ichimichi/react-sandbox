6cdc8fc2cb76a024efda1499ce52ab9d1bae37c5
ichimichi <ichimichilaribok@gmail.com>
2020-09-29 12:41:01 +0530

    extracted navbar and container

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, {Suspense, lazy} from "react";
 import ReactDOM from 'react-dom';
 import './index.css'
 
@@ -13,42 +13,56 @@ const Home = lazy(() => import('./routes/home'));
 const About = lazy(() => import('./routes/about'));
 const Game = lazy(() => import('./routes/game'));
 
-export default function App() {
+function NavBar() {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/game">Game</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <div className={"container"}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Switch>
-                            <Route path="/about" component={"About"}>
-                                <About />
-                            </Route>
-                            <Route path="/game" component={"Game"}>
-                                <Game />
-                            </Route>
-                            <Route path="/" component={"Home"}>
-                                <Home />
-                            </Route>
-                        </Switch>
-                    </Suspense>
-                </div>
-            </div>
-        </Router>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/game">Game</Link>
+                </li>
+            </ul>
+        </nav>
     );
+}
+
+function Container() {
+    return (
+        <div className={"container"}>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path="/about" component={"About"}>
+                        <About/>
+                    </Route>
+                    <Route path="/game" component={"Game"}>
+                        <Game/>
+                    </Route>
+                    <Route path="/" component={"Home"}>
+                        <Home/>
+                    </Route>
+                </Switch>
+            </Suspense>
+        </div>
+    );
+
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <NavBar/>
+                    <Container/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 ReactDOM.render(
