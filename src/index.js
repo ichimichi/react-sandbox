7946bb5a02f49e88d45782463c91743d864df835
ichimichi <ichimichilaribok@gmail.com>
2020-09-30 23:31:43 +0530

    material assets rendering

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-    <h1>Hello, World</h1>,
-  document.getElementById('root')
+    <>
+        <div className="sidebar" data-color="rose" data-background-color="black"
+             data-image="../assets/img/sidebar-1.jpg">
+            <div className="logo">
+                <a href="http://www.creative-tim.com" className="simple-text logo-mini">
+                    NI
+                </a>
+                <a href="http://www.creative-tim.com" className="simple-text logo-normal">
+                    Nice Infotech
+                </a>
+            </div>
+        </div>
+        <div className="main-panel">
+        </div>
+    </>,
+    document.getElementById('root')
 );
