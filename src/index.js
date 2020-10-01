eb390d3fb5360c0a1af038f756410d97a8844871
ichimichi <ichimichilaribok@gmail.com>
2020-10-01 14:13:32 +0530

    extracted App

--- src/index.js
+++ src/index.js
@@ -13,25 +13,33 @@ import SideBar from "./sidebar";
 const SignIn = lazy(() => import('./auth/signin'));
 const SignUp = lazy(() => import('./auth/signup'));
 
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <Router>
+                    <SideBar/>
+                    <div className="main-panel">
+                        <div>
+                            <Suspense fallback={<div>Loading...</div>}>
+                                <Switch>
+                                    <Route path="/signin">
+                                        <SignIn/>
+                                    </Route>
+                                    <Route path="/signup">
+                                        <SignUp/>
+                                    </Route>
+                                </Switch>
+                            </Suspense>
+                        </div>
+                    </div>
+                </Router>
+            </>
+        );
+    }
+}
+
 ReactDOM.render(
-    <>
-        <Router>
-            <SideBar/>
-            <div className="main-panel">
-                <div>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Switch>
-                            <Route path="/signin">
-                                <SignIn/>
-                            </Route>
-                            <Route path="/signup">
-                                <SignUp/>
-                            </Route>
-                        </Switch>
-                    </Suspense>
-                </div>
-            </div>
-        </Router>
-    </>,
+    <App/>,
     document.getElementById('root')
 );
