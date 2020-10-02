d20fe0814cbfb1e4af992b9e65fe2c6021914db9
ichimichi <ichimichilaribok@gmail.com>
2020-10-02 14:18:21 +0530

    added primer assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {
+  BackendError,
+  Lockscreen,
+  NotFound,
+  PasswordReset,
+  Signin,
+  Signup
+} from "./primer/pages";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(
-    <h1>Hello, World</h1>,
-  document.getElementById('root')
+import AppProvider from "./primer/components/AppProvider/AppProvider";
+import Dashboard from "./primer/containers/Dashboard";
+import React from "react";
+
+import { render } from "react-dom";
+
+render(
+  <AppProvider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/404" component={NotFound} />
+        <Route exact path="/500" component={BackendError} />
+        <Route exact path="/Lockscreen" component={Lockscreen} />
+        <Route exact path="/forgot" component={PasswordReset} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/signup" component={Signup} />
+        <Route path="/" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  </AppProvider>,
+  document.getElementById("root")
 );
+
